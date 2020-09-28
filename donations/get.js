39933fa5ae11400e8a17b81522af8d16489be1b5
Poh Peng <wangpp1@hotmail.com>
2020-09-28 21:22:55 +0800

    revert to working yml

--- donations/get.js
+++ donations/get.js
@@ -4,24 +4,29 @@ const dynamodb = require('./dynamodb');
 
 module.exports.get = (event, context, callback) => {
 
-  console.log("test")
-  console.log(event.pathParameters.division)
   const params = {
     TableName: process.env.DYNAMODB_TABLE,
     Key: {
       "division": event.pathParameters.division
     },
+    KeyConditionExpression: "#div = :div_value",
+    ExpressionAttributeNames:{
+        "#div": "division"
+    },
+    ExpressionAttributeValues: {
+        ":div_value": event.pathParameters.division
+    }
   };
 
   // fetch todo from the database
-  dynamodb.get(params, (error, result) => {
+  dynamodb.query(params, (error, data) => {
     // handle potential errors
     if (error) {
       console.error(error);
       callback(null, {
         statusCode: error.statusCode || 501,
         headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t fetch the donation item.',
+        body: 'Couldn\'t find the donation items matching division.',
       });
       return;
     }
@@ -29,7 +34,7 @@ module.exports.get = (event, context, callback) => {
     // create a response
     const response = {
       statusCode: 200,
-      body: JSON.stringify(result.Item),
+      body: JSON.stringify(data.Items),
     };
     callback(null, response);
   });
